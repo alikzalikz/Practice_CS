2a3430ce444ea52b10ac5cee4ae2c4f961aa1b79
Ali koleiny zadeh <A.koleinyz@gmail.com>
2022-09-09 02:02:11 +0430

    Project Switch_Example finish

--- Switch_Example/Program.cs
+++ Switch_Example/Program.cs
@@ -1,2 +1,67 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch_Example
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Please Enter Number :");
+      int NumberOfWeek = Convert.ToInt32(Console.ReadLine());
+
+      switch (NumberOfWeek)
+      {
+        case 0:
+          {
+            Console.WriteLine("Shanbe");
+            break;
+          }
+        case 1:
+          {
+            Console.WriteLine("YeckShanbe");
+          break;
+          }
+        case 2:
+          {
+            Console.WriteLine("DoShanbe");
+            break;
+          }
+        case 3:
+          {
+            Console.WriteLine("SeShanbe");
+            break;
+          }
+        case 4:
+          {
+            Console.WriteLine("ChaharShanbe");
+            break;
+          }
+        case 5:
+          {
+            Console.WriteLine("PanjShanbe");
+            break;
+          }
+        case 6:
+          {
+            Console.WriteLine("Jome");
+            break;
+          }
+        default:
+          {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Not Found!");
+            Console.ResetColor();
+            break;
+          }
+
+      }
+
+      Console.ReadKey();
+
+    }
+  }
+}
