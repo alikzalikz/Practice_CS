cb889777399fe4a216ab3cd00cc47c8e4ce32cf3
Ali koleiny zadeh <A.koleinyz@gmail.com>
2022-09-08 18:06:49 +0430

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string Name = "Ali";
+      string name = "Iman";
+      
+      int a = 4;
+      int b = 6;
+      int c = a + b;
+      c = a - b;
+      c = a * b;
+
+      Console.WriteLine(c);
+
+      Console.WriteLine(Name);
+      Console.WriteLine(name);
+      Console.ReadKey();
+    }
+  }
+
+//  class Iman
+//  {
+//      string Name = "Ali";   
+//      string name = "Iman";
+//  }
+}
---
+++ test2/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace test2;
+public class Class1
+{ public int sum(int a,int b)=>a+b;
+
+}
