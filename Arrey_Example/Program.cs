01521be590293071ffda7a91d7f1f5ce5a802b53
Ali koleiny zadeh <A.koleinyz@gmail.com>
2022-09-09 13:01:28 +0430

    project Arrey_Example finish

--- Arrey_Example/Program.cs
+++ Arrey_Example/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrey_Example
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      
+      int[] numbers = {1, 2, 3, 4}; // start counting from 0!
+
+      int number = numbers[2]; // number = 5
+
+      string[] Names = {"Reza", "Ali", "Iman"};
+
+      for(int n = 0; n <= 2; n++)
+      {
+        Console.WriteLine(Names[n]); // teach code
+
+//          string name = Names[n];  // my code
+//          Console.WriteLine(name);
+      }
+      
+      Console.ReadKey();
+
+
+    }
+  }
+}
