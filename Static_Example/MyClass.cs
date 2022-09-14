5cd24e30ef023f54875e1c7475bc0274a9987a27
Ali koleiny zadeh <A.koleinyz@gmail.com>
2022-09-15 02:25:14 +0430

    finish Static_Example project

---
+++ Static_Example/MyClass.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Example
+{
+    class MyClass
+    {
+        public string name = "null";
+        public string family = "null";
+        public static string website = "null";
+
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
