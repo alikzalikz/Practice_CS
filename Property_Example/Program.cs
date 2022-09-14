dd613f20491f0adde3a28dfcefdde165fa519df0
Ali koleiny zadeh <A.koleinyz@gmail.com>
2022-09-15 03:49:59 +0430

    finish Property_Example project

--- Property_Example/Program.cs
+++ Property_Example/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Property_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car pride = new Car();
+            pride.Speed = 120;
+            // pride.CarName = "Peykan";  // Read-Only
+            
+            System.Console.WriteLine($"Car Name Is {pride.CarName}"); 
+            System.Console.WriteLine($"Speed Is {pride.Speed}");
+        }
+    }
+}
