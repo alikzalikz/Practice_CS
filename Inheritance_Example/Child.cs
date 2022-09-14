c15574004092db0afede12e68e2693500dd488ee
Ali koleiny zadeh <A.koleinyz@gmail.com>
2022-09-15 02:51:33 +0430

    finish Inheritnce_Example project

---
+++ Inheritance_Example/Child.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_Example
+{
+    class Child:Parent
+    {
+        public static int Multiple(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
--- Inheritance_Example/Program.cs
+++ Inheritance_Example/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine(Parent.Sum(5, 6));
+            // Console.WriteLine(Parent.Minus(10, 8));
+
+            Console.WriteLine(Child.Sum(10, 8));
+            Console.WriteLine(Child.Minus(10, 8));
+            Console.WriteLine(Child.Multiple(10, 8));
+
+        }
+    }
+}
