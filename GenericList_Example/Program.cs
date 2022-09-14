925614e3047d3b89025e6052822a460e6fe15ceb
Ali koleiny zadeh <A.koleinyz@gmail.com>
2022-09-15 04:11:52 +0430

    finish GenericList_Example project

--- GenericList_Example/Program.cs
+++ GenericList_Example/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> Numbers = new List<int>();
+            Numbers.Add(5);
+            Numbers.Add(6);
+            Numbers.Add(7);
+            Numbers.Add(8);
+            Numbers.Add(9);
+            Numbers.Add(10);
+
+            Numbers.Remove(7);
+            Numbers.RemoveAt(4); // If you remove dome index, decrement number of index
+
+            List<string> Names = new List<string>();
+            Names.Add("ali");
+            Names.Add("rahim");
+            Names.Add("sara");
+            Names.Add("mahin");
+
+            foreach(string name in Names)
+            {
+                System.Console.WriteLine($"Name Is {name}");
+            }
+        }
+    }
+}
