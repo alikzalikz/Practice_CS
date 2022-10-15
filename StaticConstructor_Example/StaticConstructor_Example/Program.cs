da57a1298767daefba3a20a206c671bd8ecc4732
AliKZ | Win11 <a.koleinyz@gmail.com>
2022-10-15 15:16:48 +0330

    create StaticConstructor_Example project

---
+++ StaticConstructor_Example/StaticConstructor_Example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticConstructor_Example
+{
+    internal class Program
+    {
+    }
+}
