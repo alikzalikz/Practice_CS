b51726423e64b208b5432444da5236b113d55632
AliKZ | Win11 <a.koleinyz@gmail.com>
2022-10-15 15:15:28 +0330

    working again on Static_Example

---
+++ Static_Example/Static_Example/MyClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Example
+{
+    class MyClass
+    {
+        public string name;
+        public string family;
+        public string website;
+
+        public MyClass(String name, string family, string website)
+        {
+            this.name = name;
+            this.family = family;
+            this.website = website;
+        }
+
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ Static_Example/Static_Example/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass ali = new MyClass("ali", "koleiny zadeh", "google.com");
+            Console.WriteLine($"Name is: {ali.name}     Family is: {ali.family}     WebSite is: {ali.website}");
+
+            Console.WriteLine(MyClass.Sum(5, 6));
+            Console.WriteLine(Ali.SiteName);
+            Console.WriteLine(Ali.SiteUrl);
+            Console.ReadKey();
+        }
+    }
+}
