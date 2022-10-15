c0692ca5fdd60fd229669f0e7f32e2f4487c8ecd
AliKZ | Win11 <a.koleinyz@gmail.com>
2022-10-15 19:36:47 +0330

    finish Namespace_Example project

--- Namespace_Example/Namespace_Example/Program.cs
+++ Namespace_Example/Namespace_Example/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Namespace_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+
+            PersianCalendar persian = new PersianCalendar();
+
+            string persianDate = persian.GetYear(DateTime.Now) + "/" + persian.GetMonth(DateTime.Now) + "/" + persian.GetDayOfMonth(DateTime.Now);
+            Console.WriteLine(persianDate);
+
+            Console.ReadKey();
+        }
+
+    }
+}
