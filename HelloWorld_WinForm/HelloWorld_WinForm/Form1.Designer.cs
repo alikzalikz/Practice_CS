c06f037f9e2abdae818a402db6ebeed6b8776f5c
AliKZ | Win11 <a.koleinyz@gmail.com>
2022-10-18 20:02:01 +0330

    test WinForm application

---
+++ HelloWorld_WinForm/HelloWorld_WinForm/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace HelloWorld_WinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnSayHello = new System.Windows.Forms.Button();
+            this.lblHello = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Your Name:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(108, 13);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(238, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // btnSayHello
+            // 
+            this.btnSayHello.Location = new System.Drawing.Point(227, 40);
+            this.btnSayHello.Name = "btnSayHello";
+            this.btnSayHello.Size = new System.Drawing.Size(119, 23);
+            this.btnSayHello.TabIndex = 2;
+            this.btnSayHello.Text = "Say Hello!";
+            this.btnSayHello.UseVisualStyleBackColor = true;
+            this.btnSayHello.Click += new System.EventHandler(this.btnSayHello_Click);
+            // 
+            // lblHello
+            // 
+            this.lblHello.AutoSize = true;
+            this.lblHello.ForeColor = System.Drawing.Color.Green;
+            this.lblHello.Location = new System.Drawing.Point(12, 45);
+            this.lblHello.Name = "lblHello";
+            this.lblHello.Size = new System.Drawing.Size(0, 13);
+            this.lblHello.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(361, 75);
+            this.Controls.Add(this.lblHello);
+            this.Controls.Add(this.btnSayHello);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hello World";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnSayHello;
+        private System.Windows.Forms.Label lblHello;
+    }
+}
+
---
+++ HelloWorld_WinForm/HelloWorld_WinForm/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWorld_WinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSayHello_Click(object sender, EventArgs e)
+        {
+            if (txtName.Text != "")
+            {
+                lblHello.ForeColor = Color.Green;
+                lblHello.Text = "Hello "+ txtName.Text;
+            }
+            else
+            {
+                lblHello.ForeColor = Color.Red;
+                lblHello.Text = "Please Enter Your Name";
+            }
+
+            //MessageBox.Show("Hello " + txtName.Text);
+        }
+    }
+}
