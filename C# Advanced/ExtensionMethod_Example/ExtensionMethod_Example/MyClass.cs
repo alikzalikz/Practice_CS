94e6748123750c09f733b64542160a9f8d1aa9f0
AliKZ | Win11 <a.koleinyz@gmail.com>
2022-10-31 19:31:18 +0330

    ExtensionMethod practice

---
+++ C# Advanced/ExtensionMethod_Example/ExtensionMethod_Example/MyClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ExtensionMethod_Example
+{
+    public static class MyClass
+    {
+        public static string ToTooman(this int value)
+        {
+            return value.ToString("#,0 Tooman");
+        }
+
+        public static string ToShamsi(this DateTime value)
+        {
+            PersianCalendar pc = new PersianCalendar();
+            return pc.GetYear(value)+"/"+pc.GetMonth(value).ToString("00")+"/"+pc.GetDayOfMonth(value).ToString("00");
+        }
+    }
+}
---
+++ C# Advanced/ExtensionMethod_Example/ExtensionMethod_Example/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethod_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int amount = 25000;
+            // IQ = 1
+            Console.WriteLine(amount.ToString("#,0 Rial"));
+            // IQ = 100
+            Console.WriteLine(MyClass.ToTooman(amount));
+            Console.WriteLine(MyClass.ToTooman(789087));
+            // iQ = 100000
+            Console.WriteLine(amount.ToTooman());
+            Console.WriteLine(89823749.ToTooman());
+
+            // persian calender
+            Console.WriteLine(DateTime.Now.ToShamsi());
+            Console.ReadKey();
+        }
+    }
+}
