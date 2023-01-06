c51b2a03bde7e50ff79f7a851c576d4bbd0a4e49
AliKZ | Win11 <A.koleinyz@gmail.com>
2023-01-06 04:55:42 +0330

    practice Chapter04/Debugging & HotReloding

---
+++ CS11Dotnet07 Book/Chapter04/Debugging/Program.cs
@@ -0,0 +1,12 @@
+﻿double a = 4.5;
+double b = 2.5;
+double answer = Add(a, b);
+
+WriteLine($"{a} + {b} = {answer}");
+WriteLine("Press ENTER to end the app.");
+ReadLine();
+
+double Add(double a, double b)
+{
+    return a * b;
+}
---
+++ CS11Dotnet07 Book/Chapter04/HotReloading/Program.cs
@@ -0,0 +1,5 @@
+﻿while (true)
+{
+    WriteLine("Hello, Hot Reload!");
+    await Task.Delay(2000);
+}
