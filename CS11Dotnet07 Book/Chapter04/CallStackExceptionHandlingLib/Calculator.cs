7032266e7753e399d831e93feb23338b61b5b717
AliKZ | Win11 <A.koleinyz@gmail.com>
2023-01-07 22:24:32 +0330

    Complete Chapter04

---
+++ CS11Dotnet07 Book/Chapter04/CallStackExceptionHandlingLib/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+
+namespace CallStackExceptionHandlingLib;
+public class Calculator
+{
+    public static void Gamma()
+    {
+        WriteLine("In Gamma");
+        Delta();
+    }
+    private static void Delta()
+    {
+        WriteLine("In Delta");
+        File.OpenText("bad file path");
+    }
+}
