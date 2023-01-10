4b09e7fe7ff5654553c6cd29121de90a807523af
AliKZ | Win11 <A.koleinyz@gmail.com>
2023-01-11 00:30:40 +0330

    practice Chapter06/Making types safely reusable with generics

---
+++ CS11Dotnet07 Book/Chapter06/PacktLibrary/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace Packt.Shared;
+
+public class Person : Object
+{
+    public string? Name { get; set; }
+    public DateTime DateOfBirth { get; set; }
+
+    public void WriteToConsole()
+    {
+        WriteLine($"{Name} was born on a {DateOfBirth:dddd}.");
+    }
+}
---
+++ CS11Dotnet07 Book/Chapter06/PeopleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using Packt.Shared;
+
+Person harry = new()
+{
+    Name = "Harry",
+    DateOfBirth = new(year: 2003, month: 12, day: 26)
+};
+
+harry.WriteToConsole();
+
+System.Collections.Hashtable lookupObject = new();
+lookupObject.Add(key: 1, value: "Alpha");
+lookupObject.Add(key: 2, value: "Beta");
+lookupObject.Add(key: 3, value: "Gamma");
+lookupObject.Add(key: harry, value: "Delta");
+
+int key = 2;
+WriteLine($"key {key} has value {lookupObject[key]}");
+WriteLine($"key {harry} has value {lookupObject[harry]}");
+
+Dictionary<int, string> lookupIntString = new();
+lookupIntString.Add(key: 1, value: "Alpha");
+lookupIntString.Add(key: 2, value: "Beta");
+lookupIntString.Add(key: 3, value: "Gamma");
+lookupIntString.Add(key: 4, value: "Delta");
+key  = 3;
+WriteLine($"key {key} has value {lookupIntString[key]}");
