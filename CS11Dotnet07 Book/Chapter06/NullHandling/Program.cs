4f2eb0d96cd2cd4e8f24f18430285f202e99db0d
AliKZ | Win11 <A.koleinyz@gmail.com>
2023-01-22 02:46:33 +0330

    practice Chapter06/Working with null values

---
+++ CS11Dotnet07 Book/Chapter06/NullHandling/Program.cs
@@ -0,0 +1,28 @@
+﻿// int thisCannotBeNull = 4;
+// thisCannotBeNull = null;
+// WriteLine(thisCannotBeNull);
+
+int? thisCouldBeNull = null;
+
+WriteLine(thisCouldBeNull);
+WriteLine(thisCouldBeNull.GetValueOrDefault());
+
+thisCouldBeNull = 7;
+
+WriteLine(thisCouldBeNull);
+WriteLine(thisCouldBeNull.GetValueOrDefault());
+
+Nullable<int> thisCouldAlsoBeNull = null;
+thisCouldAlsoBeNull = 9;
+WriteLine(thisCouldAlsoBeNull);
+
+Address address = new()
+{
+    Building = null,
+    Street = null!,
+    City = "London",
+    Region = "UK"
+};
+
+WriteLine(address.Building?.Length);
+WriteLine(address.Street.Length);
