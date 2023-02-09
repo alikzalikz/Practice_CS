608286ad9442aed7edc9e1a1054435b45bde7c57
AliKZ | Win11 <A.koleinyz@gmail.com>
2023-02-10 02:31:26 +0330

    pracitce Chapter06/Writing better code

---
+++ CS11Dotnet07 Book/Chapter06/WarningsAsErrors/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Enter a name: ");
+string? name = Console.ReadLine();
+if (name == null || name == "")
+{
+    Console.WriteLine("You did not a name");
+    return;
+}
+Console.WriteLine($"Hello, {name} has {name.Length} characters!");
