8c34e8b9edf464a9e1054ba62501a0286c44b64f
AliKZ | Win11 <A.koleinyz@gmail.com>
2023-02-11 23:43:47 +0330

    practice Chapter07/Publishing your code for deployment

---
+++ CS11Dotnet07 Book/Chapter07/DotNetEverywhere/Program.cs
@@ -0,0 +1,23 @@
+﻿WriteLine("I can run everywhere!");
+WriteLine($"OS Version is {Environment.OSVersion}.");
+
+if (OperatingSystem.IsMacOS())
+{
+    WriteLine("I am macOS.");
+}
+else if (OperatingSystem.IsWindowsVersionAtLeast(major: 10, build: 22000))
+{
+    WriteLine("I am Windows 11.");
+}
+else if (OperatingSystem.IsWindowsVersionAtLeast(major: 10))
+{
+    WriteLine("I am Windows 10.");
+}
+else
+{
+    WriteLine("I am some other mysterious OS.");
+}
+WriteLine("Press ENTER to stop me.");
+ReadLine();
+
+// dotnet publish -r win10-x64 -c Release --self-contained -p:PublishSingleFile=true -p:PublishTrimmed=True -p:TrimMode=Link
