71c9c4237eb0c2897c42ce9922542e0ffef8b7b4
AliKZ | Win11 <A.koleinyz@gmail.com>
2023-02-11 16:55:35 +0330

    practice Chapter07/Understanding .NET components

---
+++ CS11Dotnet07 Book/Chapter07/AssembliesAndNamespaces/Program.cs
@@ -0,0 +1,11 @@
+﻿using System; // String
+using System.Xml.Linq; // XDocument
+
+XDocument doc = new();
+
+string s1 = "Hello";
+String s2 = "World";
+WriteLine($"{s1} {s2}");
+
+WriteLine($"int.MaxValue = {int.MaxValue}");
+WriteLine($"nint.MaxValue = {nint.MaxValue}");
---
+++ CS11Dotnet07 Book/Chapter07/SharedLibrary/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SharedLibrary
+{
+    public class Class1
+    {
+
+    }
+}
