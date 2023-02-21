96a72a443c87035cdacfeb049ae8fc4a974ed01e
AliKZ | Win11 <A.koleinyz@gmail.com>
2023-02-21 18:27:44 +0330

    practice CDB/Chapter08/Working with spans, indexes, and ranges

---
+++ CS11Dotnet07 Book/Chapter08/WorkingWithRanges/Program.cs
@@ -0,0 +1,17 @@
+﻿string name = "Samantha Jones";
+
+int lengthOfFirst = name.IndexOf(' ');
+int lengthOfLast = name.Length - lengthOfFirst - 1;
+
+string firstName = name.Substring(startIndex: 0, length: lengthOfFirst);
+string lastName = name.Substring(startIndex: name.Length - lengthOfLast, length: lengthOfLast);
+
+WriteLine($"First name: {firstName}, Last name: {lastName}");
+
+WriteLine("------------------------------");
+
+ReadOnlySpan<char> nameAsSpan = name.AsSpan();
+ReadOnlySpan<char> firstNameSpan = nameAsSpan[0..lengthOfFirst];
+ReadOnlySpan<char> lastNameSpan = nameAsSpan[^lengthOfLast..^0];
+
+WriteLine($"First name: {firstNameSpan.ToString()}, Last name: {lastNameSpan.ToString()}");
