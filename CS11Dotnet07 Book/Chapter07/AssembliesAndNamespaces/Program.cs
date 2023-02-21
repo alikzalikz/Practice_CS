fa76df1e26eebf6deaf0a5e06a8c81bd83a2307a
AliKZ | Win11 <A.koleinyz@gmail.com>
2023-02-21 12:48:14 +0330

    complete CDB/Chapter07

--- CS11Dotnet07 Book/Chapter07/AssembliesAndNamespaces/Program.cs
+++ CS11Dotnet07 Book/Chapter07/AssembliesAndNamespaces/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System; // String
 using System.Xml.Linq; // XDocument
 using Packt.Shared;
+using DialectSoftware.Collections;
+using DialectSoftware.Collections.Generics;
 
 XDocument doc = new();
 
@@ -12,5 +14,29 @@ WriteLine($"int.MaxValue = {int.MaxValue}");
 WriteLine($"nint.MaxValue = {nint.MaxValue}");
 
 Write("Enter a color value in hex: ");
-string? hex = ReadLine();
+string? hex = "00ff8c";
 WriteLine($"Is {hex} a valid color value? {hex.IsValidHex()}");
+
+Axis x = new("x", 0, 10, 1);
+Axis y = new("y", 0, 4, 1);
+Matrix<long> matrix = new(new[] { x, y });
+
+for (int i = 0; i < matrix.Axes[0].Points.Length; i++)
+{
+    matrix.Axes[0].Points[i].Label = "x" + i.ToString();
+}
+for (int i = 0; i < matrix.Axes[1].Points.Length; i++)
+{
+    matrix.Axes[1].Points[i].Label = "y" + i.ToString();
+}
+foreach (long[] c in matrix)
+{
+    matrix[c] = c[0] + c[1];
+}
+foreach (long[] c in matrix)
+{
+    WriteLine("{0},{1} ({2},{3}) = {4}",
+    matrix.Axes[0].Points[c[0]].Label,
+    matrix.Axes[1].Points[c[1]].Label,
+    c[0], c[1], matrix[c]);
+}
