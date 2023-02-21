e5822b81f99d278afafb98ceacd5a66df46760df
AliKZ | Win11 <A.koleinyz@gmail.com>
2023-02-21 08:56:02 +0330

    add SharedLibrary pakage to my project for test

--- CS11Dotnet07 Book/Chapter07/AssembliesAndNamespaces/Program.cs
+++ CS11Dotnet07 Book/Chapter07/AssembliesAndNamespaces/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System; // String
 using System.Xml.Linq; // XDocument
+using Packt.Shared;
 
 XDocument doc = new();
 
@@ -9,3 +10,7 @@ WriteLine($"{s1} {s2}");
 
 WriteLine($"int.MaxValue = {int.MaxValue}");
 WriteLine($"nint.MaxValue = {nint.MaxValue}");
+
+Write("Enter a color value in hex: ");
+string? hex = ReadLine();
+WriteLine($"Is {hex} a valid color value? {hex.IsValidHex()}");
