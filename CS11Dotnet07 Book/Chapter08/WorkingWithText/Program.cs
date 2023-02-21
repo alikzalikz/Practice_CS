880a4c8c60aa18ba06d53d10c95226c1bd39f8f6
AliKZ | Win11 <A.koleinyz@gmail.com>
2023-02-21 13:54:29 +0330

    practice CDB/Chapter08/Working with text

---
+++ CS11Dotnet07 Book/Chapter08/WorkingWithText/Program.cs
@@ -0,0 +1,38 @@
+﻿string city = "London";
+WriteLine($"{city} is {city.Length} characters long.");
+
+WriteLine($"First char is {city[0]} and fourth is {city[3]}.");
+
+
+string cities = "Paris,Tehran,Chennai,Sydney,New York,Medellín";
+
+string[] citiesArray = cities.Split(',');
+
+WriteLine($"There are {citiesArray.Length} items in the array:");
+
+foreach (string item in citiesArray)
+{
+    WriteLine(item);
+}
+
+string fullName = "Alan Shore";
+
+int indexOfTheSpace = fullName.IndexOf(' ');
+
+string firstName = fullName.Substring(startIndex: 0, length: indexOfTheSpace);
+string lastName = fullName.Substring(startIndex: indexOfTheSpace + 1);
+
+WriteLine($"Original: {fullName}");
+WriteLine($"Swapped: {lastName}, {firstName}");
+
+
+string company = "Microsoft";
+bool startsWithM = company.StartsWith("M");
+bool containsN = company.Contains("N");
+
+WriteLine($"Text: {company}");
+WriteLine($"Starts with M: {startsWithM}, contains an N: {containsN}");
+
+
+string recombined = string.Join(" => ", citiesArray);
+WriteLine(recombined);
