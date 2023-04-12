aea0c711ba2cfb2abb0f8d5095d1729e4651d439
alikz | Win11 <A.koleinyz@gmail.com>
2023-04-12 20:14:11 +0430

    practice CDB/Chapter13/Using Entity Framework Core with ASP.NET Core

--- CS11Dotnet07 Book/PracticalApps/Northwind.Web/Pages/Suppliers.cshtml.cs
+++ CS11Dotnet07 Book/PracticalApps/Northwind.Web/Pages/Suppliers.cshtml.cs
@@ -1,18 +1,43 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Packt.Shared;
+using Microsoft.AspNetCore.Mvc; // [BindProperty], IActionResult
 
 namespace Northwind.Web.Pages;
 
 public class SuppliersModel : PageModel
 {
-    public IEnumerable<string>? Suppliers { get; set; }
+    private NorthwindContext db;
+
+    public SuppliersModel(NorthwindContext injectedContext)
+    {
+        db = injectedContext;
+    }
+
+    public IEnumerable<Supplier>? Suppliers { get; set; }
 
     public void OnGet()
     {
         ViewData["Title"] = "Northwind B2B - Suppliers";
 
-        Suppliers = new[]
+        Suppliers = db.Suppliers
+            .OrderBy(c => c.Country)
+            .ThenBy(c => c.CompanyName);
+    }
+
+    [BindProperty]
+    public Supplier? Supplier { get; set; }
+
+    public IActionResult OnPost()
+    {
+        if ((Supplier is not null) && ModelState.IsValid)
         {
-            "Alpha Co", "Beta Limited", "Gamma Corp"
-        };
+            db.Suppliers.Add(Supplier);
+            db.SaveChanges();
+            return RedirectToPage("/suppliers");
+        }
+        else
+        {
+            return Page();
+        }
     }
 }
