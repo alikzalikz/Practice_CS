ede4e68731413bb57c3b1f5c4ce110f05145b79c
alikzalikz <A.koleinyz@gmail.com>
2023-04-15 12:33:13 +0430

    practice CDB/Chapter13/Configuring services and...

--- CS11Dotnet07 Book/PracticalApps/Northwind.Web/Program.cs
+++ CS11Dotnet07 Book/PracticalApps/Northwind.Web/Program.cs
@@ -3,6 +3,7 @@ using Packt.Shared; // AddNorthwindContext()
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddNorthwindContext();
+builder.Services.AddRequestDecompression();
 
 var app = builder.Build();
 
@@ -11,7 +12,27 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+app.Use(async (HttpContext context, Func<Task> next) =>
+{
+    RouteEndpoint? rep = context.GetEndpoint() as RouteEndpoint;
+
+    if (rep is not null)
+    {
+        WriteLine($"Endpoint name: {rep.DisplayName}");
+        WriteLine($"Endpoint route pattern: {rep.RoutePattern.RawText}");
+    }
+
+    if (context.Request.Path == "/bonjour")
+    {
+        await context.Response.WriteAsync("Bonjour Monde!");
+        return;
+    }
+
+    await next();
+});
+
 app.UseHttpsRedirection();
+app.UseRequestDecompression();
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
